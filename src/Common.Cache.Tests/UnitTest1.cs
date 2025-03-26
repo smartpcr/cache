7135ecc0367c7481b99fd0808f6a108d9c39f513
Xiaodong Li <xiaodoli@microsoft.com>
2025-03-26 07:40:47 -0700

    init

---
+++ src/Common.Cache.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿namespace Common.Cache.Tests;
+
+using Xunit;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ src/Common.Cache/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Common.Cache;
+
+public class Class1
+{
+
+}
